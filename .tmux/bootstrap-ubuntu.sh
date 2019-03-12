d58896211638fb7d1e61d44e5737df2b345da387
Ans <AnselmNicholas@users.noreply.github.com>
2019-03-12 07:26:54 -0700

    Update tmux bootstrap script to v2.8

--- .tmux/bootstrap-ubuntu.sh
+++ .tmux/bootstrap-ubuntu.sh
@@ -1,7 +1,7 @@
 # https://gist.github.com/P7h/91e14096374075f5316e
 # Steps to build and install tmux from source on Ubuntu.
 # Takes < 25 seconds on EC2 env [even on a low-end config instance].
-VERSION=2.7
+VERSION=2.8
 sudo apt-get -y remove tmux
 sudo apt-get -y install build-essential bc wget tar libevent-dev libncurses-dev
 wget https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz
